971f0d9ba45939f0490b5aba32f27df7a800c668
Mohit-Singh-123 <84739333+Mohit-Singh-123@users.noreply.github.com>
2024-03-06 12:14:42 +0000

    initial commit

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"// Its a way to declare that we are writing according to newer version
+// alert('hello') Gives error as Node doesn't have access to the browser's DOM where alert is commonly defined
+
+//Primitive data types are 
+//number
+//bigint
+//string
+//boolean
+//null => can also be used as standalone value
+//undefined
+//symbol used for uniquness
+
+//object
+console.log(typeof null,"jdkls") 
+console.log(typeof undefined,"asdfj")
+
---
+++ 01_basics/variables.js
@@ -0,0 +1,13 @@
+const firstName="Mohit ";
+let surName="Singh";
+var age;
+//Object
+const person={
+    name:'Mohit',
+    lastName:'Singh',
+    age:'22',
+    location:"Faridabad"
+}
+console.log(age) /*->it will result as undefined*/ 
+// var type of variable are not recommended to use asit dpn't have any control on block scope and many other reasons
+// console.table(person)
